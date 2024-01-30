c645fa0cfa1be3ff59d323a376cd1ffa32d48ee9
Gabriel Lisboa <143036931+GabrielLisboaCosta@users.noreply.github.com>
2024-01-29 22:15:16 -0300

    primeiro exercicio

---
+++ aplication/Principal.java
@@ -0,0 +1,27 @@
+package aplication;
+
+import util.CurrencyConverter;
+
+import java.util.Locale;
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Valor do dólar?");
+        double dolar = teclado.nextDouble();
+        System.out.println("Quantos reais vai pagar?");
+        double reais = teclado.nextDouble();
+
+        double total = CurrencyConverter.converter(dolar, reais);
+
+        System.out.printf("Total: %.2f%n", total);
+
+
+
+
+
+        teclado.close();
+    }
+}
---
+++ util/CurrencyConverter.java
@@ -0,0 +1,11 @@
+package util;
+
+public class CurrencyConverter {
+    public static final double iof = 0.06;
+
+    public static double converter(double dolar, double reais){
+        double semImposto =  reais * dolar;
+        double imposto = semImposto * iof;
+        return semImposto + imposto;
+    }
+}
