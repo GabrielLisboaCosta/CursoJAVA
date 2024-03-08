cf99ce53c6481b1f2f9e3779cd273a2a72f0dda7
Gabriel Lisboa <143036931+GabrielLisboaCosta@users.noreply.github.com>
2024-03-07 22:57:00 -0300

    transações

--- trabalho15_JDBC_deletar/src/application/Program.java
+++ trabalho15_JDBC_deletar/src/application/Program.java
@@ -1,31 +1,45 @@
 package trabalho15_JDBC_deletar.src.application;
 
 import trabalho12_JDBC.src.db.DB;
-import trabalho15_JDBC_deletar.src.db.DbIntegrityException;
+import trabalho13_JDBC_inserir.src.db.DbException;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
 
         try{
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                 "DELETE FROM department "
-                         + "WHERE "
-                         + "Id = ?");
 
-            st.setInt(1, 5);
+            conn.setAutoCommit(false);
 
-            int rowsAffected = st.executeUpdate();
-            System.out.println("Done! Rows affected: " + rowsAffected);
+            st = conn.createStatement();
+
+            int rwos1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+            /*int x = 1;
+            if(x < 2){
+                throw new SQLException("Fake error");
+            }*/
+
+            int rwos2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("Rows1: " + rwos1);
+            System.out.println("Rows2: " + rwos2);
         }
         catch (SQLException e){
-            throw new DbIntegrityException(e.getMessage());
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back! Caused by:" + e.getMessage());
+            } catch (SQLException ex) {
+                throw new DbException("Error trying to rollback! Cause by: " + ex.getMessage());
+            }
         }
         finally {
             DB.closeStatement(st);
